30c3dcdedfbc7acea1809058efa0cb386977ba4e
edbullen <edbullen>
2024-02-05 10:53:44 +0000

    Create the job-run notebook for Transactions File Load

--- jobs/run_test_module.py
+++ jobs/run_test_module.py
@@ -1,15 +1,17 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Execute the monthly_summary Job
-
+# MAGIC # Execute the test_module Job
+# MAGIC
 
 # COMMAND ----------
+
 from etl import test_module
 
 dbutils.widgets.text(name="catalog", defaultValue='', label='field')
 dbutils.widgets.text("schema", defaultValue='', label='field')
 
 # COMMAND ----------
+
 status = test_module.return_true()
 
 if status:
---
+++ jobs/run_transaction_file_load.py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Execute the Transactions file-load job
+
+# COMMAND ----------
+
+# import the ETL functions
+from etl import data_load
+
+# get Job parameters - ** these can be set in the Databricks Job Configuration **
+dbutils.widgets.text(name="catalog", defaultValue='', label='field')
+dbutils.widgets.text("schema", defaultValue='', label='field')
+dbutils.widgets.text("source_folder", defaultValue='', label='field')
+dbutils.widgets.text("target_table", defaultValue='', label='field')
+dbutils.widgets.text("format_type", defaultValue='', label='field')
+
+
+
+# COMMAND ----------
+
+# run autoloader job to check for new files
+data_load.auto_loader(spark
+                     , catalog=catalog
+                     , schema=schema
+                     , source_folder=source_folder
+                     , target_table=target_table
+                     , format_type=format_type)
