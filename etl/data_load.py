d532ac630183086369e13208f1c6b98c3fdfc424
Ed Bullen <ed.bullen@databricks.com>
2024-02-05 18:15:55 +0000

    merge transactions and join with fraud data into silver

--- etl/data_load.py
+++ etl/data_load.py
@@ -1,8 +1,11 @@
 # Import functions
 from pyspark.sql.functions import col, current_timestamp
+from delta.tables import *
 
-from databricks.connect import DatabricksSession
-from databricks.connect.session import DatabricksSession as SparkSession
+import pandas as pd
+
+# from databricks.connect import DatabricksSession
+# from databricks.connect.session import DatabricksSession as SparkSession
 
 
 def auto_loader(spark, catalog, schema, source_folder, target_table, format_type="csv"):
@@ -38,3 +41,53 @@ def auto_loader(spark, catalog, schema, source_folder, target_table, format_type
      .option("checkpointLocation", checkpoint_path)
      .trigger(availableNow=True)
      .toTable(table_name))
+
+
+def web_url_pull(spark, catalog, schema, url, target_table, format_type="csv", columns=None):
+    """ get data from a web-url and merge write to the target_table
+
+    :param spark     SparkSession
+    :param catalog   String - Unity catalog location for input file and output table
+    :param schema    String - Unity schema location for input file and output table
+    :param source_folder String - folder / directory name in which input file is located
+    :param target_table  String - name of output tablr
+    :param format_type   String - input file format (CSV or JSON)
+    :param columns   List - optional list of columns to filter before writing out
+
+    """
+
+    #delta_table = DeltaTable.forPath(spark, f"{catalog}.{schema}.{target_table}")
+
+    if format_type == 'csv':
+        pandas_df = pd.read_csv(url)
+        if columns:
+            pandas_df = pandas_df[columns]
+
+        df = spark.createDataFrame(pandas_df)
+
+        # Write PySpark DataFrame to Delta table
+        df.write.format("delta").mode("overwrite")\
+            .saveAsTable(f"{catalog}.{schema}.{target_table}")
+
+    else:
+        raise ValueError(f"Unhandled file format type: {format_type}")
+
+
+def load_silver_transactions(spark, catalog, schema):
+    """ Load the Silver Transactions Table based on bronze txns joined with fraud reports"""
+
+    spark.sql("""
+    MERGE INTO hsbc.hr.silver_transactions as target
+    USING (SELECT f.is_fraud, t.* EXCEPT(countryOrig, countryDest, t._rescued_data), 
+         regexp_replace(countryOrig, "--", "") as countryOrig, 
+         regexp_replace(countryDest, "--", "") as countryDest, 
+         newBalanceOrig - oldBalanceOrig as diffOrig, 
+         newBalanceDest - oldBalanceDest as diffDest
+         FROM hsbc.hr.transactions t
+        LEFT JOIN hsbc.hr.fraud_reports f USING(id)) as source
+    ON source.id = target.id
+    WHEN NOT MATCHED
+      THEN INSERT *
+    """
+    )
+
--- main.py
+++ main.py
@@ -11,8 +11,8 @@ sdk_config = sdk_connect_config()
 # DB-connect spark session on the Databricks Cluster connected to by DB-Connect
 spark = SparkSession.builder.sdkConfig(sdk_config).getOrCreate()
 
-def run_file_loader(catalog, schema, source_folder, target_table, format_type):
 
+def run_file_loader(catalog, schema, source_folder, target_table, format_type):
     print(f"Running {format_type} file loader for {catalog}.{schema}.{target_table} from {source_folder}/")
 
     data_load.auto_loader(spark
@@ -22,6 +22,28 @@ def run_file_loader(catalog, schema, source_folder, target_table, format_type):
                           , target_table=target_table
                           , format_type=format_type)
     print("file loader complete")
+
+
+def run_fraud_loader(catalog, schema, url, target_table, format_type, columns):
+    print(f"Running {format_type} URL loader for {catalog}.{schema}.{target_table} from {url}/")
+
+    data_load.web_url_pull(spark
+                           , catalog=catalog
+                           , schema=schema
+                           , url=url
+                           , target_table=target_table
+                           , format_type=format_type
+                           , columns=columns)
+
+    print("fraud loader pull from remote URL complete")
+
+
+def run_silver_load(catalog, schema):
+    print(f"Running silver transactions table loader for {catalog}.{schema}.silver_transactions")
+    data_load.load_silver_transactions(spark, catalog=catalog, schema=schema)
+    print("Silver Transactions table load complete")
+
+
 
 if __name__ == '__main__':
     # DB-Connect Example
@@ -34,8 +56,14 @@ if __name__ == '__main__':
     else:
         print("Test Module ran with: Failure")
 
-    run_file_loader("hsbc", "hr", source_folder="bank_transactions", target_table="transactions", format_type="csv")
+    #run_file_loader("hsbc", "hr", source_folder="bank_transactions", target_table="transactions", format_type="csv")
 
+    #run_fraud_loader("hsbc", "hr"
+    #                 , url="https://raw.githubusercontent.com/edbullen/dbx-bank-fraud/main/data/fraud_reports/fraud_reports_part_b.csv"
+    #                 , target_table="fraud_reports"
+    #                 , format_type="csv"
+    #                 , columns=["is_fraud", "id"])
 
+    run_silver_load("hsbc", "hr")
 
 
