c35a732ee139e44148f6bf92c2e6de5305a4706e
edbullen <edbullen>
2024-02-06 12:24:30 +0000

    extra query examples in a notebook

--- notebooks/query_transactions.py
+++ notebooks/query_transactions.py
@@ -50,5 +50,35 @@ dbutils.widgets.text("schema", defaultValue='', label='field')
 # MAGIC %sql
 # MAGIC SELECT source_file, count(*) 
 # MAGIC FROM 
-# MAGIC (SELECT * FROM ${catalog}.${schema}.transactions VERSION AS OF 1)
+# MAGIC (SELECT * FROM ${catalog}.${schema}.transactions VERSION AS OF 3)
 # MAGIC GROUP BY source_file;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from hsbc.hr.gold_transactions limit 10;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from hsbc.hr.silver_transactions LIMIT 10;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Query the Gold Transactions View
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select countryOrig , countryDest , type, value from (
+# MAGIC     select countryOrig , countryDest , type, sum(amount ) as value 
+# MAGIC     from ${catalog}.${schema}.gold_transactions
+# MAGIC     where is_fraud and amount > 350000
+# MAGIC     group by countryOrig , countryDest ,type
+# MAGIC ) order by value desc
+# MAGIC ;
+
+# COMMAND ----------
+
+
