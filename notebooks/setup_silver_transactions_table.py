3b9c7409ebf9f0f2e552a81a8a3f468486414d4f
ed.bullen@gmail.com <ed.bullen@gmail.com>
2024-05-04 16:23:07 +0000

    change silver setup script to allow different source schema and source transactions table

--- notebooks/setup_silver_transactions_table.py
+++ notebooks/setup_silver_transactions_table.py
@@ -5,16 +5,32 @@
 
 # COMMAND ----------
 
+# DBTITLE 1,Specify the source schema for building the Silver table in the Target schema
+
+# Depending on whether this is working on the Azure ADLS source demo or the Big Query demo - set this to transactions or bronze_transactions
+dbutils.widgets.text("transactions_table", defaultValue='', label='field')
+
 dbutils.widgets.text(name="catalog", defaultValue='', label='field')
 dbutils.widgets.text("schema", defaultValue='', label='field')
+dbutils.widgets.text(name="source_catalog", defaultValue='', label='field')
+dbutils.widgets.text("source_schema", defaultValue='', label='field')
+
 catalog = dbutils.widgets.get("catalog")
 schema = catalog = dbutils.widgets.get("schema")
 
+catalog = dbutils.widgets.get("source_catalog")
+schema = catalog = dbutils.widgets.get("source_schema")
+
+transactions_table = dbutils.widgets.get("transactions_table")
 
 
 # COMMAND ----------
 
-table_name = f"{catalog}.{schema}.silver_transactions"
+table_name = f"{catalog}.{schema}.{transactions_table}"
+
+# COMMAND ----------
+
+print(table_name)
 
 # COMMAND ----------
 
@@ -26,8 +42,8 @@ table_name = f"{catalog}.{schema}.silver_transactions"
 # MAGIC           regexp_replace(countryDest, "\-\-", "") as countryDest, 
 # MAGIC           newBalanceOrig - oldBalanceOrig as diffOrig, 
 # MAGIC           newBalanceDest - oldBalanceDest as diffDest
-# MAGIC FROM ${catalog}.${schema}.transactions t
-# MAGIC   LEFT JOIN ${catalog}.${schema}.fraud_reports f using(id)
+# MAGIC FROM ${source_catalog}.${source_schema}.${transactions_table} t
+# MAGIC   LEFT JOIN ${source_catalog}.${source_schema}.fraud_reports f using(id)
 # MAGIC WHERE 1 = 0;
 
 # COMMAND ----------
