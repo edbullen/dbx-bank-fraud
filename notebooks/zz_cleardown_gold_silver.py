09b3ff2686456d1c90506629e06689fe3cd4b9b0
edbullen <edbullen>
2024-10-08 14:10:49 +0000

    Fix the cleardown script for models, predictions, gold_table

--- notebooks/zz_cleardown_gold_silver.py
+++ notebooks/zz_cleardown_gold_silver.py
@@ -14,9 +14,17 @@ spark.sql(f"USE {unity_catalog}.{unity_schema}")
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## START MAIN
+# MAGIC - fraud_predictions table
+# MAGIC - bank_fraud_predict model and versions
+# MAGIC - gold_transactions view
+
+# COMMAND ----------
+
 # DBTITLE 1,Drop the Fraud Predictons Table
 # MAGIC %sql
-# MAGIC DROP TABLE fraud_predictions;
+# MAGIC DROP TABLE ${unity_catalog}.${unity_schema}.fraud_predictions;
 
 # COMMAND ----------
 
@@ -46,10 +54,18 @@ client.delete_registered_model(model_name)
 
 # DBTITLE 1,Drop the Gold View
 # MAGIC %sql
-# MAGIC DROP VIEW gold_transactions;
+# MAGIC DROP VIEW ${unity_catalog}.${unity_schema}.gold_transactions;
 
 # COMMAND ----------
 
-# DBTITLE 1,Drop the Silver Table
+# MAGIC %md
+# MAGIC ## END MAIN ##
+# MAGIC
+# MAGIC - **DELETE THE ML MODEL EXPERIMENTS MANUALLY** 
+# MAGIC - Separate cleardown for DLT
+
+# COMMAND ----------
+
+# DBTITLE 1,ONLY FOR NON DLT SETUP - Drop the Silver Table
 # MAGIC %sql
 # MAGIC DROP TABLE silver_transactions;
