3d33100bdd853fa71078cc611fd84f18b7fe96ad
edbullen <edbullen>
2024-12-17 10:38:40 +0000

    simplify catalog and schema naming in the gold txn view create script

--- notebooks/setup_gold_transactions_view.py
+++ notebooks/setup_gold_transactions_view.py
@@ -11,37 +11,37 @@
 
 # COMMAND ----------
 
-dbutils.widgets.text(name="silver_catalog", defaultValue='', label='field')
-dbutils.widgets.text("silver_schema", defaultValue='', label='field')
-
-dbutils.widgets.text(name="reference_catalog", defaultValue='', label='field')
-dbutils.widgets.text("reference_schema", defaultValue='', label='field')
+dbutils.widgets.text(name="catalog", defaultValue='', label='field')
+dbutils.widgets.text("schema", defaultValue='', label='field')
 
 
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC CREATE OR REPLACE VIEW ${silver_catalog}.${silver_schema}.gold_transactions 
+# MAGIC USE CATALOG ${catalog};
+# MAGIC USE SCHEMA ${schema};
+# MAGIC
+# MAGIC CREATE OR REPLACE VIEW gold_transactions 
 # MAGIC AS 
 # MAGIC   SELECT t.* EXCEPT(countryOrig, countryDest, is_fraud), c.* EXCEPT(id),
 # MAGIC           boolean(coalesce(is_fraud, 0)) as is_fraud,
 # MAGIC           o.alpha3_code as countryOrig, o.country as countryOrig_name, o.long_avg as countryLongOrig_long, o.lat_avg as countryLatOrig_lat,
 # MAGIC           d.alpha3_code as countryDest, d.country as countryDest_name, d.long_avg as countryLongDest_long, d.lat_avg as countryLatDest_lat
-# MAGIC FROM ${silver_catalog}.${silver_schema}.silver_transactions t
-# MAGIC   INNER JOIN ${reference_catalog}.${reference_schema}.country_coordinates o ON t.countryOrig=o.alpha3_code 
-# MAGIC   INNER JOIN ${reference_catalog}.${reference_schema}.country_coordinates d ON t.countryDest=d.alpha3_code 
-# MAGIC   INNER JOIN ${reference_catalog}.${reference_schema}.banking_customers c ON c.id=t.customer_id 
+# MAGIC FROM silver_transactions t
+# MAGIC   INNER JOIN country_coordinates o ON t.countryOrig=o.alpha3_code 
+# MAGIC   INNER JOIN country_coordinates d ON t.countryDest=d.alpha3_code 
+# MAGIC   INNER JOIN banking_customers c ON c.id=t.customer_id 
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC DESC ${silver_catalog}.${silver_schema}.gold_transactions 
+# MAGIC DESC ${catalog}.${schema}.gold_transactions 
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT count(*) FROM ${silver_catalog}.${silver_schema}.gold_transactions;
+# MAGIC SELECT count(*) FROM ${catalog}.${schema}.gold_transactions;
 # MAGIC
 # MAGIC
 # MAGIC
